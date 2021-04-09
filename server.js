696876e415f300502deffb9f42f708a7f454eab7
Glitch (steep-thin-tornado) <none>
2021-04-09 13:10:40 +0000

    🍐🙅 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,34 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require("express");
+const app = express();
+
+// our default array of dreams
+
+
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/home.html");
+});
+app.get("/unit3", (request, response) => {
+  response.sendFile(__dirname + "/views/unit3.html");
+});
+// send the default array of dreams to the webpage
+app.get("/unit2", (request, response) => {
+  response.sendFile(__dirname + "/views/unit2.html");
+});
+app.get("/index", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
